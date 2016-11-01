f1edafa597fd58fc0e9a9d45efc7ba64072c9802
Nijat Zeynalov <nijat12@gmail.com>
2016-11-01 13:33:50 -0400

    First creation

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sharksApp
+ * @description
+ * # sharksApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sharksApp', [
+    'ngAnimate'
+  ]);
