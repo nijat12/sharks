b3d45f0a7fe318a601139bec97a743469d1ab612
Nijat Zeynalov <nijat12@gmail.com>
2016-11-01 16:58:58 -0400

    Page ready

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,13 +9,39 @@
  * Main module of the application.
  */
 var app = angular
-  .module('shark', ['ui.router'])
+  .module('shark', ['ui.router', 'ui.bootstrap'])
 
 
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$uibModal', '$scope', function ($uibModal, $scope) {
+
+    $scope.contact = {name : '', email: '', message: ''};
+
+
+
+    $scope.openMail = function () {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'views/mail.html',
+        backdrop: true,
+        scope: $scope
+      });
+    };
+
+    $scope.openBuy = function () {
+      var modalInstance2 = $uibModal.open({
+        animation: true,
+        templateUrl: 'views/buy.html',
+        backdrop: true,
+        scope: $scope
+      });
+    };
+
+    $scope.openSubscribe = function () {
+      var modalInstance3 = $uibModal.open({
+        animation: true,
+        templateUrl: 'views/subscribe.html',
+        backdrop: true,
+        scope: $scope
+      });
+    };
+  }]);
